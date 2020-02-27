5b3dae9da572d487271f73721999e64cd3286a0c
Neverender <tldavis09@yahoo.com>
2020-02-26 19:35:10 -0600

    added client id & client secret - testing gitignore

--- utils/api.js
+++ utils/api.js
@@ -1,14 +1,16 @@
 const axios = require("axios");
+require("dotenv").config();
 
 const api = {
 	getUser(username) {
-		const queryUrl = `https://api.github.com/users/${username}`;
-		console.log(queryUrl);
-
-		axios.get(queryUrl).then(function(result) {
-			const user = result.data;
-			console.log(user);
-		});
+		return axios
+			.get(
+				`https://api.github.com/users/${username}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`
+			)
+			.catch((err) => {
+				console.log(`User not found`);
+				process.exit(1);
+			});
 	}
 };
 
