9462e3f607dba001279b35ff5a0950b51c188bc4
Neverender <tldavis09@yahoo.com>
2020-02-26 18:00:07 -0600

    added prompted questions

--- index.js
+++ index.js
@@ -4,7 +4,54 @@ const inquirer = require("inquirer");
 const api = require("./utils/api");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-const questions = [];
+function promptUser() {
+	return inquirer.prompt([
+		{
+			type: "input",
+			name: "username",
+			message: "What is your Github username?"
+		},
+
+		{
+			type: "input",
+			name: "repo",
+			message: "What is the repo name of your project?"
+		},
+
+		{
+			type: "input",
+			name: "description",
+			message: "Please provide a short description of your project"
+		},
+
+		{
+			type: "input",
+			name: "license",
+			message: "What kind of license should your project have?"
+		},
+
+		{
+			type: "input",
+			name: "install",
+			message: "What command should be run to install dependencies?"
+		},
+
+		{
+			type: "input",
+			name: "contributing",
+			message:
+				"What do users need to know about contributing to the repo?"
+		},
+
+		{
+			type: "input",
+			name: "test",
+			message: "What command should be run to test?"
+		}
+	]);
+}
+
+promptUser();
 
 function writeToFile(fileName, data) {}
 
